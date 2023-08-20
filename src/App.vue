43108b71e9e63ed815716be82cfcfd98277b0cf8
Salman Farcy <salmanfarcy@gmail.com>
2023-08-20 23:45:57 +0600

    done

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import ParentComponent from './components/ParentComponent.vue'
+
+</script>
+
+<template> 
+<h2 class="text-blue-900 text-xl font-bold my-3 strong"> OSTAD VUE.JS - ASSIGNMENT-8 </h2>
+    <ParentComponent/>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,18 @@
+<template>
+       <h3 class="text-blue-700 text-xl font-bold my-5">This Is Child Component</h3>
+       <p class="my-5">
+         {{ modelValue }}
+      </p>
+      <button class="bg-blue-500 text-white font-bold py-2 px-2 w-30" @click="$emit('update:modelValue', msgEmit())">
+         Child Button
+       </button>
+</template>
+
+<script setup>
+ const propsValue  = defineProps(['modelValue'])
+ defineEmits(['update:modelValue'])
+
+ function msgEmit(){
+     return 'HI THERE! THIS MESSAGE FROM CHILD';  
+ }
+</script>
